4bd8dad8e9049fafae5177fddcd6b28e1b7c6f17
lisniuse <17560235@qq.com>
2019-01-09 17:53:04 +0800

    update.

---
+++ teadocs.config.js
@@ -0,0 +1,24 @@
+'use strict';
+const path = require('path')
+
+module.exports = {
+    doc: {
+        name: "Web之幸运",
+        description: "",
+        version: "",
+        dir: "",
+        outDir: "",
+        staticDir: ""
+    }, 
+    theme: {
+        dir: "", 
+        title: "",
+        headHtml: "",
+        footHtml: "",
+        isMinify: true,
+        rootPath: "/"
+    },
+    nav: {
+        tree: "./tree"
+    }
+}
